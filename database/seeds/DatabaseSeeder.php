3d6f3ac29c4fde9694398900e1de871e7b5b0c77
desenv <cristiano.m.watanabe@gmail.com>
2019-05-13 20:47:03 -0300

    criado seeder para criar usuario ADM

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name'=>'ADM',
+            'email'=>'adm@adm.com',
+            'login'=>'adm',
+            'password'=>bcrypt('123')
+        ]);
     }
 }
